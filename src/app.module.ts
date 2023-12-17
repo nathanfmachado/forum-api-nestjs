5f2b3a223bb4544855b53486047b13d7a650c9c7
Nathan Fabiano Machado <nathan.machado@taqtile.com.br>
2023-12-17 10:43:24 -0300

    Configure env using nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { CreateAccountController } from './controllers/create-account.controller';
 import { CryptoService } from './crypto/crypto.service';
+import { envSchema } from './env';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (envConfig) => envSchema.parse(envConfig),
+      isGlobal: true,
+    }),
+  ],
   controllers: [CreateAccountController],
   providers: [AppService, PrismaService, CryptoService],
 })
